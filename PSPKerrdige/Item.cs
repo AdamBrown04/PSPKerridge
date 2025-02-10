8cb5fd4cc9099da720bcbfa2ecda635b7c3c27f5
Lordsj1234 <simonlaujones@outlook.com>
2025-02-10 15:57:24 +0000

    Created Item Class

---
+++ PSPKerrdige/Item.cs
@@ -0,0 +1,28 @@
+﻿namespace PSPKerrdige;
+
+public class Item
+{
+    public float Mass { get; set; }
+    public float Volume { get; set; }
+    public float Width { get; set; }
+    public float Height { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    
+    public Item(float mass, float volume, float width, float height, string name, string type)
+    {
+        Mass = mass;
+        Volume = volume;
+        Width = width;
+        Height = height;
+        Name = name;
+        Type = type;
+    }
+    
+    public string PrintItem()
+    {
+        return $"{Name} Type: {Type} Mass: {Mass} Volume: {Volume} Width: {Width} Height: {Height}";
+    }
+    
+    
+}
