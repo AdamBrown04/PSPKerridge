8d0fd7d4b3b32ac322be2f6cbb8e536d7bca0592
AdamBrown04 <adambrown2004@outlook.com>
2025-02-19 12:04:36 +0000

    Updated UI so it shows total amount of lorries seperate to the full solution

--- PSPKerrdige/HomeForm.Designer.cs
+++ PSPKerrdige/HomeForm.Designer.cs
@@ -36,14 +36,17 @@ namespace PSPKerrdige
             lbl_FileSelected = new Label();
             txb_FilePath = new TextBox();
             txb_Solution = new RichTextBox();
+            lbl_Solution = new Label();
+            txb_NumOfLorries = new TextBox();
             SuspendLayout();
             // 
             // btn_FileSelect
             // 
             btn_FileSelect.BackColor = SystemColors.Info;
+            btn_FileSelect.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
             btn_FileSelect.Location = new Point(632, 45);
             btn_FileSelect.Name = "btn_FileSelect";
-            btn_FileSelect.Size = new Size(353, 55);
+            btn_FileSelect.Size = new Size(353, 70);
             btn_FileSelect.TabIndex = 1;
             btn_FileSelect.Text = "Select File";
             btn_FileSelect.UseVisualStyleBackColor = false;
@@ -53,9 +56,10 @@ namespace PSPKerrdige
             // 
             btn_FileSave.BackColor = SystemColors.Info;
             btn_FileSave.Enabled = false;
-            btn_FileSave.Location = new Point(632, 106);
+            btn_FileSave.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_FileSave.Location = new Point(632, 121);
             btn_FileSave.Name = "btn_FileSave";
-            btn_FileSave.Size = new Size(353, 55);
+            btn_FileSave.Size = new Size(353, 70);
             btn_FileSave.TabIndex = 2;
             btn_FileSave.Text = "Save Output to File";
             btn_FileSave.UseVisualStyleBackColor = false;
@@ -64,9 +68,10 @@ namespace PSPKerrdige
             // btn_ExitProgram
             // 
             btn_ExitProgram.BackColor = SystemColors.Info;
-            btn_ExitProgram.Location = new Point(632, 469);
+            btn_ExitProgram.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_ExitProgram.Location = new Point(632, 506);
             btn_ExitProgram.Name = "btn_ExitProgram";
-            btn_ExitProgram.Size = new Size(353, 71);
+            btn_ExitProgram.Size = new Size(353, 70);
             btn_ExitProgram.TabIndex = 3;
             btn_ExitProgram.Text = "Exit Program";
             btn_ExitProgram.UseVisualStyleBackColor = false;
@@ -75,9 +80,10 @@ namespace PSPKerrdige
             // btn_CalculateLoads
             // 
             btn_CalculateLoads.BackColor = SystemColors.Info;
-            btn_CalculateLoads.Location = new Point(632, 392);
+            btn_CalculateLoads.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_CalculateLoads.Location = new Point(632, 430);
             btn_CalculateLoads.Name = "btn_CalculateLoads";
-            btn_CalculateLoads.Size = new Size(353, 71);
+            btn_CalculateLoads.Size = new Size(353, 70);
             btn_CalculateLoads.TabIndex = 4;
             btn_CalculateLoads.Text = "Calculate Optimum Loading Method";
             btn_CalculateLoads.UseVisualStyleBackColor = false;
@@ -110,12 +116,31 @@ namespace PSPKerrdige
             txb_Solution.TabIndex = 7;
             txb_Solution.Text = "";
             // 
+            // lbl_Solution
+            // 
+            lbl_Solution.AutoSize = true;
+            lbl_Solution.Location = new Point(12, 552);
+            lbl_Solution.Name = "lbl_Solution";
+            lbl_Solution.Size = new Size(168, 20);
+            lbl_Solution.TabIndex = 8;
+            lbl_Solution.Text = "Total Amount of Lorries:";
+            // 
+            // txb_NumOfLorries
+            // 
+            txb_NumOfLorries.Location = new Point(186, 549);
+            txb_NumOfLorries.Name = "txb_NumOfLorries";
+            txb_NumOfLorries.ReadOnly = true;
+            txb_NumOfLorries.Size = new Size(84, 27);
+            txb_NumOfLorries.TabIndex = 9;
+            // 
             // HomeForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ControlLight;
-            ClientSize = new Size(992, 546);
+            ClientSize = new Size(992, 581);
+            Controls.Add(txb_NumOfLorries);
+            Controls.Add(lbl_Solution);
             Controls.Add(txb_Solution);
             Controls.Add(txb_FilePath);
             Controls.Add(lbl_FileSelected);
@@ -140,5 +165,7 @@ namespace PSPKerrdige
         private Label lbl_FileSelected;
         private TextBox txb_FilePath;
         private RichTextBox txb_Solution;
+        private Label lbl_Solution;
+        private TextBox txb_NumOfLorries;
     }
 }
