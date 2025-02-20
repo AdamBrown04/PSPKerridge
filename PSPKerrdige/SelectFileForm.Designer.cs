1ed93dddcb264da798097d482eb7a7a4f0f71f45
AdamBrown04 <adambrown2004@outlook.com>
2025-02-20 10:43:36 +0000

    made it so the SelectFileForm cannot be resized

--- PSPKerrdige/SelectFileForm.Designer.cs
+++ PSPKerrdige/SelectFileForm.Designer.cs
@@ -54,9 +54,7 @@
             btn_FileConfirm.TabIndex = 1;
             btn_FileConfirm.Text = "Confirm File";
             btn_FileConfirm.UseVisualStyleBackColor = false;
-
             btn_FileConfirm.Click += btn_FileConfirm_Click;
-
             // 
             // btn_Return
             // 
@@ -80,9 +78,7 @@
             // 
             // txb_FileSelected
             // 
-
             txb_FileSelected.Enabled = false;
-
             txb_FileSelected.Location = new Point(112, 73);
             txb_FileSelected.Name = "txb_FileSelected";
             txb_FileSelected.ReadOnly = true;
@@ -100,6 +96,7 @@
             Controls.Add(btn_Return);
             Controls.Add(btn_FileConfirm);
             Controls.Add(btn_FileFind);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
             Name = "SelectFileForm";
             Text = "Select File";
             ResumeLayout(false);
