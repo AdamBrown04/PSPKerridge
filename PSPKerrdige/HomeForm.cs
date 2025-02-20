4886afeb05aa0c67c422b4cc18d04b509d1c9198
AdamBrown04 <adambrown2004@outlook.com>
2025-02-20 10:07:45 +0000

    added additional comments to the program to explain the logic of the program at certain points

--- PSPKerrdige/HomeForm.cs
+++ PSPKerrdige/HomeForm.cs
@@ -39,14 +39,15 @@ namespace PSPKerrdige
 
                     ItemSort itemSort = new ItemSort { Items = items };
 
+                    //default values for the lorry maximum weight and volume (in CM^3)
                     float weight = 10000f;
                     float volume = 109760000f;
 
                     itemSort.FirstFitDecreasing(weight, volume);
 
                     ItemSwap itemSwap = new ItemSwap { Items = items, Lorries = itemSort.Lorries };
-
-                    int iterations = 500;
+                    
+                    int iterations = 500000;
                     float finalFitnessValue = itemSwap.HillClimbing(iterations);
 
                     btn_FileSave.Enabled = true;
@@ -62,15 +63,18 @@ namespace PSPKerrdige
 
         private void btn_FileSave_Click(object sender, EventArgs e)
         {
+            //creates an instance of the FolderBrowserDialog class and sets the initial directory to C drive
             FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
             folderBrowserDialog.InitialDirectory = "c:\\";
-
+            //opens the users folder browser
             folderBrowserDialog.ShowNewFolderButton = true;
             DialogResult result = folderBrowserDialog.ShowDialog();
             if (result == DialogResult.OK)
             {
+                //saves the location of the folder selected by the user 
                 string folderPath = folderBrowserDialog.SelectedPath;
                 string fileName = "solution.txt";
+                //creates an instance of the FileStream class and writes the solution to the file selected by user
                 FileStream outputSolution = new FileStream(folderPath + "\\" + fileName, FileMode.Create);
                 StreamWriter writer = new StreamWriter(outputSolution);
                 writer.Write(txb_Solution.Text);
