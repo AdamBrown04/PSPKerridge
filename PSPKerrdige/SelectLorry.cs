b9d4e77225d6819c07c9e787b0ec464b14fafee1
AdamBrown04 <adambrown2004@outlook.com>
2025-03-24 14:11:11 +0000

    added ability to the SelectLorry form to save information about that specific vehicle

--- PSPKerrdige/SelectLorry.cs
+++ PSPKerrdige/SelectLorry.cs
@@ -8,6 +8,7 @@ namespace PSPKerrdige
     public partial class SelectLorry : Form
     {
         private List<Lorry> SortedLorrys;
+        int index = 0;
 
         public SelectLorry(List<Lorry> sortedLorrys)
         {
@@ -20,11 +21,11 @@ namespace PSPKerrdige
         {
             if (int.TryParse(Nud_lorryNumber.Text, out int lorryNumber))
             {
-                int index = lorryNumber - 1;
+                index = lorryNumber - 1;
 
                 if (index >= 0)
                 {
-                    txb_ItemDetails.Text = SortedLorrys[index].DisplayResults();
+                    txb_LorryDetails.Text = SortedLorrys[index].DisplayResults();
                 }
                 else
                 {
@@ -40,11 +41,37 @@ namespace PSPKerrdige
         private void btn_ShowLorry_Click(object sender, EventArgs e)
         {
             DisplayLorryItems();
+            btn_saveLorry.Enabled = true;
         }
 
         private void ReturnToMenu_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btn_saveLorry_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.InitialDirectory = "c:\\";
+            folderBrowserDialog.ShowNewFolderButton = true;
+            DialogResult result = folderBrowserDialog.ShowDialog();
+            string fileName = $"Lorry{index}.txt";
+            if (result == DialogResult.OK)
+            {
+                string folderPath = folderBrowserDialog.SelectedPath;                
+                FileStream outputSolution = new FileStream(folderPath + "\\" + fileName, FileMode.Create);
+                StreamWriter writer = new StreamWriter(outputSolution);
+                writer.Write(txb_LorryDetails.Text);
+                writer.Close();
+                outputSolution.Close();
+                MessageBox.Show($"Solution saved to {fileName}", "Success", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show($"Failed to save solution to {fileName}", "Error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
         }
     }
 }
